99f8dcd67011581905dc567c8a239151ea8de540
Augusto Camolesi <camolesi2121@hotmail.com>
2019-09-19 12:38:59 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/trustevents/EventManagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.trustevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+}
